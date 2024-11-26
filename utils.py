f2ab6f58a29bb1a49995ffbc65e4b1ead8e16363
qianlinyi <327120052@qq.com>
2024-11-26 11:47:12 +0800

    upload code

--- utils.py
+++ utils.py
@@ -25,9 +25,18 @@ def generate_csv(src_path: str, dataset: str):
             'im_Parabasal': 3,
             'im_Superficial-Intermediate': 4
         },
+        # 'herlev': {
+        #     'normal': 0,
+        #     'abnormal': 1,
+        # },
         'herlev': {
-            'normal': 0,
-            'abnormal': 1,
+            'carcinoma_in_situ': 0,
+            'light_dysplastic': 1,
+            'moderate_dysplastic': 2,
+            'normal_columnar': 3,
+            'normal_intermediate': 4,
+            'normal_superficiel': 5,
+            'severe_dysplastic': 6,
         },
         'mendeley': {
             'High squamous intra-epithelial lesion': 0,
@@ -40,7 +49,7 @@ def generate_csv(src_path: str, dataset: str):
     for cls in os.listdir(src_path):
         for img in os.listdir(os.path.join(src_path, cls)):
             data.append((os.path.join(src_path, cls, img),
-                        class2index[dataset][cls]))
+                         class2index[dataset][cls]))
     print(f'The total number of images: {len(data)}')
     df = pd.DataFrame(data, columns=['path', 'class'])
     df = shuffle(df)  # random shuffle
@@ -82,19 +91,19 @@ def create_model(model_name: str, output: int):
         model = tf.keras.applications.inception_resnet_v2.InceptionResNetV2(
             include_top=False,
             weights='imagenet',
-            input_shape=(256,256,3)
+            input_shape=(256, 256, 3)
         )
     elif model_name == 'InceptionV3':
         model = tf.keras.applications.inception_v3.InceptionV3(
             include_top=False,
             weights='imagenet',
-            input_shape=(256,256,3)
+            input_shape=(256, 256, 3)
         )
     elif model_name == 'Xception':
         model = tf.keras.applications.Xception(
             include_top=False,
             weights='imagenet',
-            input_shape=(256,256,3)
+            input_shape=(256, 256, 3)
         )
     else:
         return None
